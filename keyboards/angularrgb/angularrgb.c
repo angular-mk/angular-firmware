7213a77a2e8e3d29d7d0c2ead0339a9c3ef0b10f
Henry Jiang <henryjiang@protonmail.com>
2019-04-27 17:17:26 -0700

    Added Angular rgb

---
+++ keyboards/angularrgb/angularrgb.c
@@ -0,0 +1,1 @@
+#include "angularrgb.h"
---
+++ keyboards/angularrgb/config.h
@@ -0,0 +1,50 @@
+#ifndef CONFIG_H
+
+#define CONFIG_H
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x6060
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    dztech
+#define PRODUCT         AngularRGB
+#define DESCRIPTION     Keyboard
+
+/* key matrix size */
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 12
+
+/* key matrix pins */
+#define MATRIX_ROW_PINS { D5, D3, D2, B7 }
+#define MATRIX_COL_PINS { D4, D6, D7, B4, F0, F1, F4, F5, F6, F7, C7, C6 }
+#define UNUSED_PINS
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION ROW2COL
+
+/* number of backlight levels */
+
+#ifdef BACKLIGHT_PIN
+#define BACKLIGHT_LEVELS 0
+#endif
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCING_DELAY 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+#define RGB_DI_PIN B0
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 30
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+#define RGBLIGHT_LIMIT_VAL 100
+
+#endif
